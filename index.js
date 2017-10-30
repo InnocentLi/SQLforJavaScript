84e29736deae4dd9786497f0b40226e4a880f5d1
李文昊 <vincentlieie@gmail.com>
2017-10-30 12:12:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var TableName;
 var num = 0;
+function qcreate(){
+    $('#test').val("create table student name age sex teacher");
+}
+function qinsert(){
+    $('#test').val("insert into student values wang 18 男 flp");
+}
+
 function init(){
     var str = $('#test').val();
 
@@ -15,11 +22,23 @@ function init(){
 //    else if(sql.match(createtable)){
 //     CreateTable();
 //    } 
+   
     if(str.indexOf("create")==0&&str.indexOf("table")==7){
         Create(str);           
     }else if(str.match("insert")){
         Insert(str);
+    }else if(str.match("delete")){
+        Delete(str);
+    }else if(str.match("drop")){
+        Drop(str);
+    }else if(str.match("select")){
+       
+    }else if(str.match("alter")){
+        Alter(str);
     }
+    
+    $('#test').val(null);
+    Update();
 }
 
 function Create(str){
@@ -31,7 +50,7 @@ function Create(str){
     $('#sql').append("<table id='"+tb[0]+"'"+"border='1'></table>");
     $('#'+tb[0]).append('<tr id='+tb[0]+'head'+'></tr>');
     for(var i = 1; i<tb.length;i++){
-      $('#'+tb[0]+'head').append("<td style='width: 60px;'>"+tb[i]+"</td>;");  
+      $('#'+tb[0]+'head').append("<th style='width: 60px;'>"+tb[i]+"</th>;");  
     }
 }
 
@@ -49,16 +68,93 @@ function Insert(str){
     }
 }
 
+function Delete(str){
+    //DELETE * FROM table_name  
+    //DELETE FROM Person WHERE LastName = 'Wilson' 
+    ShowRecord("执行delete语句:"+str);
+    var tb3 = new Array();
+    tb3 = str.split(" ");
+    if(tb3[1]=="*"){
+        console.log(tb3);
+        console.log($("#"+tb3[3]+" tr td"));
+     $("#"+tb3[3]+" tr td").remove(); 
+    }
+    else if(tb3[1]=="from"){
+
+        // console.log("tb3");
+        // console.log(tb3);
+        // console.log($("#"+tb3[2]+" tr"));
+        // console.log($("#"+tb3[2]+" tr td"));
+       
+        //  while($("#"+tb3[2]+" tr td").val()==tb3[6]){
+        //      console,log(this);
+        //     aim = this;          
+        //   console.log(aim.index());
+        //  } 
+       // $("div")
+        var a; 
+        a = $("#"+tb3[2]+" tr th:contains('"+tb3[4]+"')").index();
+        // console.log($("#"+tb3[2]+" tr th"))
+        console.log(a);
+        a = a+1; 
+        // console.log($("#"+tb3[2]+" tr th:contains('sex')"));
+        //$("#table3 tr:gt(0):eq(1)").hide();
+       // $("#table3 tr td:nth-child(2)")
+     // $("#"+tb3[2]+" tr td:nth-child(2)").hide();
+      //  console.log($("#"+tb3[2]+" tr td:nth-child("+a+")"));
+       console.log(a);
+     //  $("#"+tb3[2]+" tr td:nth-child("+a+")")
+     var aim = $("#"+tb3[2]+" tr td:contains('"+tb3[6]+"')");
+     console.log(aim);
+     aim.parent().hide();
+    }
+    
+}
+
+ function Update(){
+   $("table td").click(function () {
+  //  console.log($("table td"));
+    var a = $('#test').val();
+    $(this).html(a);
+    var tdSeq = $(this).parent().find("td").index($(this));
+    var trSeq = $(this).parent().parent().find("tr").index($(this).parent());
+     ShowRecord("第" + (trSeq) + "行，第" + (tdSeq+1) + "列");
+ })
+
+ }
+
+function Select(str){
+
+}
+
+function Alter(str){
+   // ALTER TABLE table_name ADD column_name 
+   //ALTER TABLE table_name DROP  column_name
+   var tb = new Array();
+   tb = str.split(" "); 
+   if(tb[3]=="add"){
+  //  $('#'+tb[0]+'head').append("<th style='width: 60px;'>"+tb[i]+"</th>;");  
+  $('#'+tb[2]+'head').append("<th style='width: 60px;'>"+tb[4]+"</th>;");  
+    }
+//else if(tb[3]=="drop"){
+     
+//    }
+
+   
+}
+
+function Drop(str){
+   // DROP TABLE 表名称
+   var tb7 = new Array();
+   tb7 = str.split(" "); 
+   $("#"+tb7[2]).remove(); 
+}
+
 function ShowRecord(str){
     
      $('#record').append('<p>'+str+new Date()+'</p>');
 }
 function OperationalDatabases(){
   //  $('#record').append('<p>'+str+new Date()+'</p>');
-}
-
-function CreateTable(){
-
-    console.log("执行CreeatTable");
 }
 
